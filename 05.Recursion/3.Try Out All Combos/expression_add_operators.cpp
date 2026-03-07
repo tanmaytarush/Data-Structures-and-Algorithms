5d81534265fe832d72dc26f315d9005125ece8a7
Tanmay Dikshit <tanmay.dikshit@pidilite.vc>
2026-03-07 18:59:42 +0530

    modified:   .DS_Store         new file:   01.Arrays/.DS_Store         new file:   02.Binary Search/.DS_Store         new file:   03.Strings/.DS_Store         new file:   04.Linked List/.DS_Store         new file:   05.Recursion/.DS_Store         new file:   05.Recursion/3.Try Out All Combos/expression_add_operators         modified:   05.Recursion/3.Try Out All Combos/expression_add_operators.cpp         new file:   06.Bit Manipulation/.DS_Store         renamed:    06.Bit Manipulation/1. Learn Bit Manipulation/01.Bit Manipulation.cpp -> 06.Bit Manipulation/1. Learn Bit Manipulation/bit_manipulation.cpp         new file:   07.Stack and Queues/.DS_Store         new file:   08. Sliding Window/.DS_Store         new file:   09. Heaps/.DS_Store         new file:   10. Greedy Approach/.DS_Store         new file:   11. Binary Trees/.DS_Store         new file:   12. Binary Search Trees/.DS_Store         new file:   13. Graphs/.DS_Store         new file:   14. Dynamic Programming/.DS_Store         new file:   15. Tries/.DS_Store

--- 05.Recursion/3.Try Out All Combos/expression_add_operators.cpp
+++ 05.Recursion/3.Try Out All Combos/expression_add_operators.cpp
@@ -20,7 +20,6 @@ Example 3:
 Input: num = "3456237490", target = 9191
 Output: []
 Explanation: There are no expressions that can be created from "3456237490" to evaluate to 9191.
- 
 
 Constraints:
 
@@ -38,9 +37,72 @@ num consists of only digits.
 #include<algorithm>
 using namespace std;
 
+class Solution
+{
+    public:
+    void solve(int index, string num, int target, long value, long prev, string path, vector<string>&ans)
+    {
+        if(index == num.size())
+        {
+            if(value == target) ans.push_back(path);
+            return;    
+        }
+
+        long curr = 0;
+        for(int i=index; i<num.size(); ++i)
+        {
+            if(i>index && num[index]=='0') // if number '00123' we stop exploring further numbers if 0 exists
+            {
+                break;
+            }
+            
+            curr = curr*10 + (num[i] - '0');
+            LOG(curr); // curr value changes based on index and i pointers
+            if(index == 0)
+            {
+                solve(i+1, num, target, curr, curr, to_string(curr), ans);
+            }
+
+            else
+            {
+                // addition operation
+                solve(i+1, num, target, value + curr, curr, path+"+"+to_string(curr), ans);
+
+                // substraction operation
+                solve(i+1, num, target, value - curr, -curr, path+"-"+to_string(curr), ans);
+
+                // multiplication operation
+                solve(i+1, num, target, value-prev + prev*curr, prev*curr, path+"*"+to_string(curr), ans);
+            }
+        }
+    }
+
+    vector<string> addOperators(string num, int target)
+    {
+        vector<string> ans;
+        solve(0, num, target, 0, 0, "", ans);
+        return ans;
+    }
+};
+
 
 
 int main()
 {
-    
+    Solution sol;
+
+    string num;
+    getline(cin, num);
+
+    int target;
+    cin>>target;
+
+    vector<string> result = sol.addOperators(num, target);
+
+    for(auto it : result)
+    {
+        cout<<it<<endl;
+    }
+
+    return 0;
 }
