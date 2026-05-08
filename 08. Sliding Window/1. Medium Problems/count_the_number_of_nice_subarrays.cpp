3e09a54f921ac72cff66de5cf645cae5dfe5a805
Tanmay Dikshit <tanmay.dikshit@pidilite.vc>
2026-05-08 17:55:29 +0530

    new file:   08. Sliding Window/1. Medium Problems/count_the_number_of_nice_subarrays         modified:   08. Sliding Window/1. Medium Problems/count_the_number_of_nice_subarrays.cpp

--- 08. Sliding Window/1. Medium Problems/count_the_number_of_nice_subarrays.cpp
+++ 08. Sliding Window/1. Medium Problems/count_the_number_of_nice_subarrays.cpp
@@ -1,7 +1,7 @@
 /*Given an array of integers nums and an integer k, a continuous subarray is called nice if there are k odd numbers in it. Return the number of nice subarrays.
 
 Example 1:
-Input: nums = [1,1,2,1,1], k = 3
+Input: nums = [1,1,2,1,1], k = 3 ; Binary array = [1, 1, 0, 1, 1]
 Output: 2
 Explanation: The only subarrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
 
@@ -15,9 +15,75 @@ APPROACH:
 4. Return ans, which represents the number of subarrays with at most k odd numbers.
 5. In the numberOfSubarrays function, return the difference between atMostK(nums, k) and atMostK(nums, k - 1) to get the number of nice subarrays.
 
+DIRECT TWO POINTERS AND SLIDING WINDOW WORKS FOR KEYWORDS LIKE ATMOST, ETC. TO FIND THE EXACT
+SUMMATION SUBARRAYS, THIS APPROACH IS THE STANDARD.
+
 CODE:*/
 
+#define LOG(x) cerr<<#x<<" "<<x<<endl;
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
 
+class Solution
+{
+    public:
+    int niceSubarrays(vector<int>&nums, int k) // return count(=k) - count(<=k)
+    {
+        return maxSubarrays(nums, k) - maxSubarrays(nums, k-1);
+    }
+
+    int maxSubarrays(vector<int>&nums, int k) // sum <= k
+    {
+        int n = nums.size();
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int count = 0;
+
+        while(end < n)
+        {
+            sum += nums[end]%2;
+
+            while(start<=end && sum>k)
+            {
+                sum -= nums[start];
+                start++;
+            }
+
+            count += (end - start + 1);
+            end++;
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    
+    int n;
+    cin>>n;
+
+    vector<int> nums(n);
+    for(int i=0; i<n; ++i)
+    {
+        cin>>nums[i];
+    }
+
+    int k;
+    cin>>k;
+
+    int result = sol.niceSubarrays(nums, k);
+
+    cout<<result;
+
+    return 0;
+}
 
 /*
 COMPLEXITY ANALYSIS:
